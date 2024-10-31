7d98c22e5c1b46852b5ea77adfc0faa0a52144ba
Hicham DIAB <diabhicham987@gmail.com>
2024-10-31 01:31:33 +0100

    Refactor UsersList component to add delete functionality

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,41 +1,65 @@
 'use client'
 
-import { addUser, fetchUsers } from '@/server-call/users.call'
+import { addUser, fetchUsers, removeUser } from '@/server-call/users.call'
 import { CreateUserDTO, User } from '@/types/user.type'
 import { useMutation, UseMutationResult, useQuery, useQueryClient, useSuspenseQuery } from '@tanstack/react-query'
+import { useState } from 'react'
 
 export function UserList() {
   const { data, isLoading, error } = useQuery({
     queryKey: ['users'],
     queryFn: fetchUsers,
   })
-  const queryClient = useQueryClient();
-  const {
-    mutate: addingUser,
-    isPending: isAddingUser,
-    error: addingError,
-  }: UseMutationResult<User, Error, CreateUserDTO> = useMutation({
+  const queryClient = useQueryClient()
+  const { mutate: addingUser, isPending: isAddingUser }: UseMutationResult<User, Error, CreateUserDTO> = useMutation({
     mutationFn: addUser,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['users'],
-      });
+      })
     },
-  }
-)
+  })
+  const [deletingUserId, setDeletingUserId] = useState<number | null>(null)
+
+  const { mutate: deleteUser, isPending: isDeletingUser }: UseMutationResult<User, Error, number> = useMutation({
+    mutationFn: removeUser,
+
+    onMutate: (userId: number) => {
+      setDeletingUserId(userId)
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['users'],
+      })
+      setDeletingUserId(null)
+    },
+
+    onError: () => {
+      setDeletingUserId(null)
+    },
+  })
 
   if (isLoading) return <p>Loading...</p>
   if (error) return <p>Error: {error.message}</p>
-
   return (
     <div className="space-y-3">
-      <ul>
+      <ul className="w-[20%] space-y-2">
         {data?.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="flex justify-between">
+            <span>{user.name}</span>
+
+            <b
+              className="cursor-pointer font-bold text-md hover:text-red-500 transition-colors"
+              onClick={() => deleteUser(user.id)}
+            >
+              {isDeletingUser && user.id === deletingUserId ? 'Deleting...' : 'X'}
+            </b>
+          </li>
         ))}
       </ul>
       <button className="py-2 px-3 border bg-red-400 rounded-lg" onClick={() => addingUser({ name: 'ahmed USMA' })}>
-      {isAddingUser ? 'Adding User...' : 'Add User'}
+        {isAddingUser ? 'Adding User...' : 'Add User'}
       </button>
     </div>
   )
@@ -46,31 +70,56 @@ export function UserListSus() {
     queryKey: ['users'],
     queryFn: fetchUsers,
   })
-  const queryClient = useQueryClient();
-  const {
-    mutate: addingUser,
-    isPending: isAddingUser,
-    error: addingError,
-  }: UseMutationResult<User, Error, CreateUserDTO> = useMutation({
+  const queryClient = useQueryClient()
+  const { mutate: addingUser, isPending: isAddingUser }: UseMutationResult<User, Error, CreateUserDTO> = useMutation({
     mutationFn: addUser,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['users'],
-      });
+      })
     },
-  }
-)
+  })
+
+  const [deletingUserId, setDeletingUserId] = useState<number | null>(null)
+
+  const { mutate: deleteUser, isPending: isDeletingUser }: UseMutationResult<User, Error, number> = useMutation({
+    mutationFn: removeUser,
+
+    onMutate: (userId: number) => {
+      setDeletingUserId(userId)
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['users'],
+      })
+      setDeletingUserId(null)
+    },
+
+    onError: () => {
+      setDeletingUserId(null)
+    },
+  })
 
   if (!isFetching && error) return <p>Error: {error.message}</p>
 
   return (
     <div className="space-y-3">
-      <ul>
+      <ul className="w-[20%] space-y-2">
         {data?.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="flex justify-between">
+            <span>{user.name}</span>
+
+            <b
+              className="cursor-pointer font-bold text-md hover:text-red-500 transition-colors"
+              onClick={() => deleteUser(user.id)}
+            >
+              {isDeletingUser && user.id === deletingUserId ? 'Deleting...' : 'X'}
+            </b>
+          </li>
         ))}
       </ul>
-      <button className="py-2 px-3 border bg-red-400 rounded-lg" onClick={() => addingUser({ name: 'Moh MCA' })}>
+      <button className="py-2 px-3 border bg-red-400 rounded-lg" onClick={() => addingUser({ name: 'ahmed USMA' })}>
         {isAddingUser ? 'Adding User...' : 'Add User'}
       </button>
     </div>
--- src/server-call/users.call.ts
+++ src/server-call/users.call.ts
@@ -8,16 +8,30 @@ export async function fetchUsers(): Promise<User[]> {
   return data
 }
 
-export async function addUser (newUser: CreateUserDTO ): Promise<User> {
+export async function addUser(newUser: CreateUserDTO): Promise<User> {
   const response = await fetch(getApiUrl('/api/users'), {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN_SECRET}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN_SECRET}`,
     },
     body: JSON.stringify(newUser),
   })
   if (!response.ok) throw new Error('Network response was not ok')
   const data = await response.json()
   return data
-} 
+}
+
+export async function removeUser(id: number): Promise<User> {
+  const response = await fetch(getApiUrl(`/api/users/${id}`), {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN_SECRET}`,
+    },
+  })
+
+  if (!response.ok) throw new Error('Network response was not ok')
+  const data = await response.json()
+  return data
+}
